fc74d858566c64982275e66aa2a9e6620b6f3557
Imdadul Haque Ohi <93985458+ohi20034@users.noreply.github.com>
2023-01-23 14:45:53 +0600

    Add files via upload

--- number theory/BinaryExponentiation.cpp
+++ number theory/BinaryExponentiation.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9 + 7;
 int binaryExpo(int pow,int base)
 {
-    if(pow==0){
+    if(pow==0)
+    {
         return 1;
     }
     if(pow%2==0)
     {
-        auto res = binaryExpo(pow/2,base);
-        return (res*res);
+        auto res = binaryExpo(pow/2,base)%MOD;
+        return (res*res)%MOD;
     }
-    return (base*binaryExpo(pow-1,base));
+    return (base%MOD*(binaryExpo(pow-1,base)%MOD))%MOD;
 }
 int main()
 {
     int power,base;
     cin >> base >> power;
-    cout << binaryExpo(power,base) <<'\n';
+    cout << binaryExpo(power,base)%MOD <<'\n';
 }
